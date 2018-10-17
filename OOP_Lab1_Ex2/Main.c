c8069c3e489eaa1a1c210c184c2654c7ff3d14cd
Dumitru Cristina-Nicoleta <crs_nikoll23@yaho.com>
2018-10-17 14:42:24 +0300

    merge

--- OOP_Lab1_Ex2/Main.c
+++ OOP_Lab1_Ex2/Main.c
@@ -3,9 +3,9 @@
 int main(void) {
 	Complex Number1, Number2, rezultat;
 
-	printf_s("Enter the first number: ");
+	printf_s("Enter the first number: \n");
 	Write(&Number1);
-	printf_s("Enter the second number: ");
+	printf_s("Enter the second number: \n");
 	Write(&Number2);
 
 	Difference(Number1, Number2, &rezultat);
--- OOP_Lab1_Ex2/Source.c
+++ OOP_Lab1_Ex2/Source.c
@@ -8,7 +8,7 @@ void Print(Complex rezultat) {
 		printf_s("+ %di", rezultat.img);
 	}
 	else if (rezultat.img < 0) {
-		printf_s("- %di", rezultat.img);
+		printf_s("%di", rezultat.img);
 	}
 	else
 		printf_s("0");
@@ -18,7 +18,7 @@ void Write(Complex *rezultat) {
 	printf_s("The real part is: ");
 	scanf_s("%d", &(rezultat->real));
 	printf_s("The imaginary part is: ");
-	scanf_s("$d", &(rezultat->img));
+	scanf_s("%d", &(rezultat->img));
 }
 
 void Difference(Complex FirstNumber, Complex SecondNumber, Complex *rezultat) {
